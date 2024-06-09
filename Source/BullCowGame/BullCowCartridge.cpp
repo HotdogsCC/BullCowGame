f82cbbca2735732f65f3f3d9adce8f51cbd24e8f
Charlie Campbell <chacha9669@hotmail.com>
2024-06-09 21:13:02 +1000

    Hidden word logic

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,33 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    SetupGame();
+    PrintLine(TEXT("Hi there, and welcome to Bull Cows."));
+    PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+    PrintLine(TEXT("Enter your guess:"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("yesss"));
+    }
+    else
+    {
+        if (HiddenWord.Len() != Input.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+        }
+
+        PrintLine(TEXT("no."));
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("urinating");
+    Lives = 5;
 }
