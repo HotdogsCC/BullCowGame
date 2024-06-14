d6d3334e12496ecb9df023b7c2f8a30b0e72db10
Charlie Campbell <chacha9669@hotmail.com>
2024-06-14 14:57:23 +1000

    Progress

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,32 +5,79 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     SetupGame();
-    PrintLine(TEXT("Hi there, and welcome to Bull Cows."));
-    PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
-    PrintLine(TEXT("Enter your guess:"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-
-    if (Input == HiddenWord)
+    if (bGameOver == true)
     {
-        PrintLine(TEXT("yesss"));
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        if (HiddenWord.Len() != Input.Len())
-        {
-            PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
-        }
-
-        PrintLine(TEXT("no."));
-    }
+        ProcessGuess(Input);  
+    }    
 }
 
 void UBullCowCartridge::SetupGame()
 {
     HiddenWord = TEXT("urinating");
-    Lives = 5;
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+    PrintLine(TEXT("Hi there, and welcome to Bull Cows."));
+    PrintLine(TEXT("The hidden word is %i characters long."), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives."), Lives);
+    PrintLine(TEXT("Enter your guess:"));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to continue"));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess)
+{
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("Good stuff, that was the hidden word!"));
+        EndGame(); 
+    }
+    else
+    {
+        if(!IsIsogram(Guess))
+        {
+            PrintLine(TEXT("No repeating letters. Try again."));
+            return;
+        }
+
+
+        if (HiddenWord.Len() != Guess.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i characters long.."), HiddenWord.Len());
+        }
+        else
+        {
+            PrintLine(TEXT("Yikes, nah, not quite"));
+            PrintLine(TEXT("You have lost a life."));
+            PrintLine(TEXT("You are now on %i lives"), --Lives); 
+        }
+
+        if(Lives <= 0)
+        {
+            PrintLine(TEXT("You lose!"));
+            EndGame();
+        }
+        else
+        {
+            PrintLine(TEXT("Try again:"));
+        }
+    }
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const
+{
+
+    return true;
 }
